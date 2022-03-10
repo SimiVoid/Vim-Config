043a53c0cf70e20cdc41a291e42448e319df8c85
SimiVoid <simivoid@gmail.com>
2022-03-10 14:01:48 +0100

    Add file exist checking

--- init.sh
+++ init.sh
@@ -11,5 +11,10 @@ vim +'PlugInstall --sync' +qa
 
 SOURCE_TEXT='source "$HOME/.vim/plugged/gruvbox/gruvbox_256palette.sh"'
 
-grep -qxF "$SOURCE_TEXT" ~/.bashrc || echo "$SOURCE_TEXT" >> ~/.bashrc
-grep -qxF "$SOURCE_TEXT" ~/.zshrc || echo "$SOURCE_TEXT" >> ~/.zshrc
+if [ -f "~/.bashrc" ]; then
+    grep -qxF "$SOURCE_TEXT" ~/.bashrc || echo "$SOURCE_TEXT" >> ~/.bashrc
+fi
+
+if [ -f "~/.zshrc" ]; then
+    grep -qxF "$SOURCE_TEXT" ~/.zshrc || echo "$SOURCE_TEXT" >> ~/.zshrc
+fi
